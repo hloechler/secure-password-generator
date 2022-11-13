086a6e975ba101416921eaf00a9ccf9f90348c94
Hailey Loechler <haileyloechler@gmail.com>
2022-11-13 10:58:30 -0800

    when the button is clicked, now the prompts will appear

--- assets/script.js
+++ assets/script.js
@@ -7,10 +7,34 @@ var specialChar =  ["!","#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", "
 var choicesCriteria = [ ]
 var passwordLength = ''
 
+
+// Write password to the #password input
+function writePassword() {
+  var getPrompts = passwordCriteria();
+  var passwordText = document.querySelector("#password");
+  //Generates prompts when the user clicks the button
+  if (getPrompts){
+    var newPassword = generatePassword();
+     passwordText.value = newPassword;
+    }
+  }
+   
+
+//Creates a password that matches the users input from the forms
+function generatePassword() {
+    var password = "";
+    for(var i = 0; i < passwordLength; i++){
+        var randomIndex = Math.floor(Math.random() * choicesCriteria.length);
+        password = password + choicesCriteria[randomIndex]
+    }
+    return password;
+}
+
 //Criteria to use for password
 var passwordCriteria = function(){
     //Prompt to ask user how long the password should be
     passwordLength = Number(prompt("How long would you like your password (8-128 characters)?"))
+
     if (passwordLength < 8 || passwordLength > 128){
         window.alert('Sorry the password must be between 8-128 characters.')
         passwordCriteria();
@@ -29,31 +53,9 @@ var passwordCriteria = function(){
     if(confirm("Would you like to use special characters? (Press cancel if no)")){
         choicesCriteria = choicesCriteria.concat(specialChar);
     }
+    return true;
 }
-
-
-
-// Write password to the #password input
-function writePassword() {
-  var passwordText = document.querySelector("#password");
-  var newPassword = generatePassword();
-    passwordText.value = newPassword;
-
-
-}
-
-function generatePassword() {
-    var password = "";
-    for(var i = 0; i < passwordLength; i++){
-        var randomIndex = Math.floor(Math.random() * choicesCriteria.length);
-        password = password + choicesCriteria[randomIndex]
-    }
-    return password;
-}
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-typeof(passwordLength);
-passwordCriteria();
